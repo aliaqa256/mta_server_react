2d43623cdfcd85ea7e28f59ad88fa65d21c5d973
aliaqa@newwin10 <aliaqa@newwin10.com>
2022-07-28 01:24:20 +0430

    first theme

--- mta/src/components/Auth/Register.js
+++ mta/src/components/Auth/Register.js
@@ -1,37 +1,64 @@
 // register user component function
-
+import styles from "./Register.module.css";
 
 const Register = ( props ) => {
     const { handleSubmit, pristine, submitting } = props;
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6 m-auto">
-                    <h1 className="display-4 text-center">Sign Up</h1>
-                    <p className="lead text-center">Create your DevConnector account</p>
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input type="text" className="form-control form-control-lg" name="name" placeholder="Name" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" className="form-control form-control-lg" name="email" placeholder="Email" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" className="form-control form-control-lg" name="password" placeholder="Password" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="confirmPassword">Confirm Password</label>
-                            <input type="password" className="form-control form-control-lg" name="confirmPassword" placeholder="Confirm Password" />
-                        </div>
-                        <button type="submit" className="btn btn-primary btn-block btn-lg" disabled={pristine || submitting}>Sign Up</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    );
+			<div className="container">
+				<div className="row">
+					<div className="col-md-6 m-auto">
+						<h1 className={` ${styles.color_white}  display-4 text-center  `}>
+							ثبت نام
+						</h1>
+						<form onSubmit={handleSubmit} className="">
+							<div className="form-group my-2">
+								<label htmlFor="name">username</label>
+								<input
+									type="text"
+									className="form-control form-control-lg"
+									name="name"
+									placeholder="یوزر نیم"
+								/>
+							</div>
+							<div className="form-group my-2">
+								<label htmlFor="email">Email</label>
+								<input
+									type="email"
+									className="form-control form-control-lg"
+									name="Email"
+									placeholder="ایمیل"
+								/>
+							</div>
+							<div className="form-group my-2">
+								<label htmlFor="password">Password</label>
+								<input
+									type="password"
+									className="form-control form-control-lg"
+									name="password"
+									placeholder="پسورد"
+								/>
+							</div>
+							<div className="form-group my-2">
+								<label htmlFor="confirmPassword">Confirm Password</label>
+								<input
+									type="password"
+									className="form-control form-control-lg"
+									name="confirmPassword"
+									placeholder="تایید پسورد"
+								/>
+							</div>
+							<button
+								type="submit"
+								className="btn btn-primary btn-block btn-lg form-control register-btn my-4 "
+								disabled={pristine || submitting}
+							>
+								ثبت نام
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
 }
 
     
